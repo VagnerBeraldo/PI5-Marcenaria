479fd9e31bee6da6af4920c958ce182cdd60c5a3
vagnerberaldo <vagnerberaldo1978@gmail.com>
2026-03-01 01:24:21 -0300

    feat: inclusão de rotas de navegação

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import PlanoDeCorte from './pages/PlanoDeCorte';
 import MenuMarcenaria from './pages/MenuMarcenaria';
 
 import './App.css'; 
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<MenuMarcenaria />} />
+        <Route path="/plano-de-corte" element={<PlanoDeCorte />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
-    <div className="app-container">
-      <MenuMarcenaria />
-      {/* <PlanoDeCorte /> */}
-    </div>
+    <Router>
+      <div className="app-container">
+        <AnimatedRoutes />
+      </div>
+    </Router>
   );
 }
 
