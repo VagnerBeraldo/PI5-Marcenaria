6e3b0dc5cb320f711146a60f2b5fdbc7b5e06797
vagnerberaldo <vagnerberaldo1978@gmail.com>
2026-03-01 15:21:12 -0300

    styles: aumento da transition de 0,4 para 0,5

--- src/components/Animation/PageTransition.jsx
+++ src/components/Animation/PageTransition.jsx
@@ -8,7 +8,7 @@ const PageTransition = ({ children, className = "" }) => {
       initial={{ opacity: 0, x: -40 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: -40 }}
-      transition={{ duration: 0.4, ease: "easeOut" }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
     >
       {children}
     </MotionNav>
